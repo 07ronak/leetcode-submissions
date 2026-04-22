8b6441f6a6d92982e22b9241fd35b98a49d77c12
Ronak Hingonia <158851792+07ronak@users.noreply.github.com>
2026-04-22 10:28:10 +0530

    Time: 22 ms (20%), Space: 58.1 MB (45%) - LeetHub

---
+++ 2452-words-within-two-edits-of-dictionary/2452-words-within-two-edits-of-dictionary.js
@@ -0,0 +1,35 @@
+/**
+ * @param {string[]} queries
+ * @param {string[]} dictionary
+ * @return {string[]}
+ */
+var twoEditWords = function(queries, dict) {
+    let set = new Set(dict)
+    let len = dict[0].length
+    let ans = []
+
+    for(let q of queries){
+        if(set.has(q)){
+            ans.push(q)
+            continue
+        }
+
+        for(let word of dict){
+            let count = 0
+
+            for(let i=0; i<len; i++){
+                if(word[i]!==q[i]){
+                    count++
+                }
+            }
+
+            if(count<=2){
+                ans.push(q)
+                break
+            }
+
+        }
+    }
+
+    return ans
+};
