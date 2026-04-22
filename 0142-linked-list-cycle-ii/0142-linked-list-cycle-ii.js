668ddb0c5339527fa2df34fb0f80dab8c047396b
Ronak Hingonia <158851792+07ronak@users.noreply.github.com>
2026-04-22 21:02:57 +0530

    Time: 49 ms (53.88%), Space: 56.7 MB (77.8%) - LeetHub

---
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.js
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var detectCycle = function(head) {
+    let fast = head
+    let slow = head
+
+    while(fast && fast.next){
+        fast = fast.next.next
+        slow = slow.next
+        if(slow===fast){
+            break
+        }
+    }
+
+    if(!fast || !fast.next) return null
+
+    fast = head
+
+    while(fast!==slow){
+        fast = fast.next
+        slow = slow.next
+    }
+
+    return fast
+};
