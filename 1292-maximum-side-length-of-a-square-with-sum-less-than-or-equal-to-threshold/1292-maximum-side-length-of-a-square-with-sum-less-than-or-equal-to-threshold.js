165f2003a9da040ec297e89eb2596b92f6ec276a
Ronak Hingonia <158851792+07ronak@users.noreply.github.com>
2026-04-23 15:27:22 +0530

    Time: 9 ms (100%), Space: 63.1 MB (80.77%) - LeetHub

--- 1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.js
+++ 1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.js
@@ -6,34 +6,34 @@
 var maxSideLength = function(mat, threshold) {
     const rows = mat.length
     const cols = mat[0].length
-    let prefix = Array.from({length: rows+1},()=>new Array(cols+1).fill(0))
 
-    for(let i=1; i<=rows; i++){
-        for(let j=1; j<=cols; j++){
-            prefix[i][j] = prefix[i-1][j] + prefix[i][j-1] + mat[i-1][j-1] - prefix[i-1][j-1]
+    let pre = Array.from({length: rows+1},()=> new Array(cols+1).fill(0))
+    //console.log(pre)
+
+    for(let r=1; r<=rows; r++){
+        for(let c=1; c<=cols; c++){
+            pre[r][c] = mat[r-1][c-1] + pre[r-1][c] + pre[r][c-1] - pre[r-1][c-1]
         }
     }
+    //console.log(pre)
+    let max = 0
+    const min = Math.min(rows,cols)
 
-    let max = Math.min(rows,cols)
-    let ans = 0
-
-    for(let size = 1; size<=max; size++){
-        for(let i=0; i<rows-size+1; i++){
-            let found = false
-            for(let j=0; j<cols-size+1; j++){
-                let sum = prefix[i+size][j+size] - prefix[i][j+size] - prefix[i+size][j] + prefix[i][j]
-
+    for(let size = 1; size<=min; size++){
+        for(let r = 0; r+size<=rows; r++){
+            for(let c = 0; c+size<=cols; c++){
+                let sum = pre[r+size][c+size] - pre[r][c+size] - pre[r+size][c] + pre[r][c]
                 if(sum<=threshold){
-                    ans = size
-                    found = true
+                    max = size
                     break
-                }  
+                }
             }
-            if(found){
+            if(max===size){
                 break
             }
         }
+        if(max!==size) break
     }
 
-    return ans
+    return max
 };
