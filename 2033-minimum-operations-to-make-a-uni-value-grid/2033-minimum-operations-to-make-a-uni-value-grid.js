04b8baa48a109f4c6cc5d9acc4377e61fabe90c0
Ronak Hingonia <158851792+07ronak@users.noreply.github.com>
2026-04-28 17:36:22 +0530

    Time: 128 ms (83.33%), Space: 79.1 MB (100%) - LeetHub

--- 2033-minimum-operations-to-make-a-uni-value-grid/2033-minimum-operations-to-make-a-uni-value-grid.js
+++ 2033-minimum-operations-to-make-a-uni-value-grid/2033-minimum-operations-to-make-a-uni-value-grid.js
@@ -1,27 +1,21 @@
-/**
- * @param {number[][]} grid
- * @param {number} x
- * @return {number}
- */
 var minOperations = function(grid, x) {
     let arr = []
+    const rem = grid[0][0] % x
 
-    for(let row of grid){
-        for(let val of row){
+    for (let row of grid) {
+        for (let val of row) {
+            if (val % x !== rem) return -1
             arr.push(val)
         }
     }
 
     arr.sort((a,b)=>a-b)
 
-    const rem = (arr[0] % x)
-    const n = arr.length
-    const mid = arr[Math.floor(n/2)]
+    const mid = arr[Math.floor(arr.length / 2)]
 
     let count = 0
-    for(let val of arr){
-        count += Math.abs(mid-val) / x
-        if((val%x)!==rem) return -1
+    for (let val of arr) {
+        count += Math.abs(mid - val) / x
     }
 
     return count
