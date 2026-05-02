f7bb35861e9b94f6e727b28f42064b6cdaee4ab0
Ronak Hingonia <158851792+07ronak@users.noreply.github.com>
2026-05-02 12:38:26 +0530

    Time: 7 ms (60.34%), Space: 53.8 MB (98.28%) - LeetHub

--- 0788-rotated-digits/0788-rotated-digits.js
+++ 0788-rotated-digits/0788-rotated-digits.js
@@ -1,24 +1,28 @@
-/**
- * @param {number} n
- * @return {number}
- */
 var rotatedDigits = function(n) {
-    let set1 = new Set([2,5,6,9])
-    let set2 = new Set([0,1,2,5,6,8,9])
+    const change = new Set([2,5,6,9]);
+    const valid = new Set([0,1,2,5,6,8,9]);
 
-    let count = 0
+    let count = 0;
 
-    for(let i=1; i<=n; i++){
-        let num = i
-        let diff = false
-        while(num){
-            let digit = num % 10
-            if(!set2.has(digit)) break
-            if(set1.has(digit)) diff = true
-            num = Math.floor(num / 10)
+    for (let i = 1; i <= n; i++) {
+        let num = i;
+        let diff = false;
+
+        while (num > 0) {
+            let digit = num % 10;
+
+            if (!valid.has(digit)) {
+                diff = false;
+                break;
+            }
+
+            if (change.has(digit)) diff = true;
+
+            num = Math.floor(num / 10);
         }
-        if(num===0 && diff===true) count++
+
+        if (num === 0 && diff) count++;
     }
 
-    return count
+    return count;
 };
