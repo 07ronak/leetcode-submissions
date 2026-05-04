7be5ca23c731eed55517fc5b6bb21deb7264fbcf
Ronak Hingonia <158851792+07ronak@users.noreply.github.com>
2026-05-04 13:19:47 +0530

    Time: 0 ms (100%), Space: 53.6 MB (88.25%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.js
+++ 0048-rotate-image/0048-rotate-image.js
@@ -3,23 +3,21 @@
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
 var rotate = function(matrix) {
-    const n = matrix.length
-
-    let l = 0;
-    let r = n-1;
+    let l = 0
+    let r = matrix.length -1
 
     while(r>l){
         for(let i=0; i<r-l; i++){
-            const top= l;
+            const top = l
             const bottom = r
 
-            const topLeft = matrix[top][l+i] //store
+            const topLeft = matrix[top][l+i]
 
-            matrix[top][l+i] = matrix[bottom-i][l] //bottom left to top left
+            matrix[top][l+i] = matrix[bottom-i][l]
 
-            matrix[bottom-i][l] = matrix[bottom][r-i] //bottom right to bottom left
+            matrix[bottom-i][l] = matrix[bottom][r-i]
 
-            matrix[bottom][r-i] = matrix[top+i][r] //top right to bottom right
+            matrix[bottom][r-i] = matrix[top+i][r]
 
             matrix[top+i][r] = topLeft
         }
