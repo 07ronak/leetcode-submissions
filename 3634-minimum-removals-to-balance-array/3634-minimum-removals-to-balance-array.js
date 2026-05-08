96ed239ffdb46cb0c37ed38e5a3a16ba79d7499b
Ronak Hingonia <158851792+07ronak@users.noreply.github.com>
2026-05-08 14:51:55 +0530

    Time: 77 ms (92.86%), Space: 71.4 MB (94.64%) - LeetHub

--- 3634-minimum-removals-to-balance-array/3634-minimum-removals-to-balance-array.js
+++ 3634-minimum-removals-to-balance-array/3634-minimum-removals-to-balance-array.js
@@ -6,9 +6,6 @@
 var minRemoval = function(nums, k) {
     nums.sort((a, b) => a - b);
     const n = nums.length;
-    
-    if(nums[0]*k >= nums[n-1]) return 0
-
     let i = 0;
     for (let j = 1; j < n; j++){
         if (i <= j && nums[j] > nums[i] * k){
