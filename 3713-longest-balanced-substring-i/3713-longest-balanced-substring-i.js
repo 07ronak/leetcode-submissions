9f8ed6e7323a3b698cefded584adc93afa614261
Ronak Hingonia <158851792+07ronak@users.noreply.github.com>
2026-05-11 15:12:01 +0530

    Time: 81 ms (80.56%), Space: 62.1 MB (36.11%) - LeetHub

--- 3713-longest-balanced-substring-i/3713-longest-balanced-substring-i.js
+++ 3713-longest-balanced-substring-i/3713-longest-balanced-substring-i.js
@@ -1,16 +1,20 @@
 var longestBalanced = function (s) {
-    const n = s.length;
-    let res = 0;
+    const n = s.length
+    if(n<3) return n
+
+    let res = 2 
 
     for (let i = 0; i < n; i++) {
+        if(res >= (n-i)) break
+
         const cnt = new Array(26).fill(0);
 
         for (let j = i; j < n; j++) {
             let flag = true;
             const c = s.charCodeAt(j) - 97;
-            cnt[c]++;
+            let val = ++cnt[c]
             for (const x of cnt) {
-                if (x > 0 && x !== cnt[c]) {
+                if (x > 0 && x !== val) {
                     flag = false;
                     break;
                 }
