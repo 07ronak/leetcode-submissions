be02a6ac68780f8d9d8259b67fb3b9a04815afdb
Ronak Hingonia <158851792+07ronak@users.noreply.github.com>
2026-05-14 15:59:20 +0530

    Time: 15 ms (81.82%), Space: 67.7 MB (90.91%) - LeetHub

--- 1674-minimum-moves-to-make-array-complementary/1674-minimum-moves-to-make-array-complementary.js
+++ 1674-minimum-moves-to-make-array-complementary/1674-minimum-moves-to-make-array-complementary.js
@@ -1,25 +1,31 @@
-var minMoves = function (nums, limit) {
-    const n = nums.length;
-    const diff = new Array(2 * limit + 2).fill(0);
+/**
+ * @param {number[]} nums
+ * @param {number} limit
+ * @return {number}
+ */
+var minMoves = function(nums, limit) {
+    const max = 2*limit
+    const n = nums.length
+    const diff = new Array(max+2).fill(0)
 
-    for (let i = 0; i < n / 2; i++) {
-        const a = Math.min(nums[i], nums[n - 1 - i]);
-        const b = Math.max(nums[i], nums[n - 1 - i]);
+    for(let i=0; i<n/2; i++){
+        const a = Math.min(nums[i],nums[n-1-i])
+        const b = Math.max(nums[i],nums[n-1-i])
 
-        diff[2] += 2;
-        diff[a + 1] -= 1;
-        diff[a + b] -= 1;
-        diff[a + b + 1] += 1;
-        diff[b + limit + 1] += 1;
+        diff[2] += 2
+        diff[a+1] -= 1
+        diff[a+b] -=1
+        diff[a+b+1] += 1
+        diff[b+limit+1] +=1
     }
 
-    let minOps = n;
-    let currentOps = 0;
+    let minOps = n
+    let curr = 0
 
-    for (let c = 2; c <= 2 * limit; c++) {
-        currentOps += diff[c];
-        minOps = Math.min(minOps, currentOps);
+    for(let i=2; i<=max; i++){
+        curr += diff[i]
+        minOps = Math.min(minOps,curr)
     }
 
-    return minOps;
+    return minOps
 };
