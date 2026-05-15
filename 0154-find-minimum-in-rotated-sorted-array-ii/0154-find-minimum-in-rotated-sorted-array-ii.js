598ba717ebd27414f48e6ffab74b9025d867107a
Ronak Hingonia <158851792+07ronak@users.noreply.github.com>
2026-05-15 12:40:13 +0530

    Time: 0 ms (100%), Space: 54 MB (60.94%) - LeetHub

---
+++ 0154-find-minimum-in-rotated-sorted-array-ii/0154-find-minimum-in-rotated-sorted-array-ii.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMin = function(nums) {
+    let left = 0
+    let right = nums.length-1
+
+    while(right>left){
+        const mid = Math.floor((left+right)/2)
+
+        if(nums[right]>nums[mid]){
+            right = mid
+        } else if(nums[right]<nums[mid]){
+            left = mid+1
+        } else{
+            right--
+        }
+    }
+
+    return nums[left]
+};
