4397116f5660c3eb7e5e369ebedd8879c3133a76
Ronak Hingonia <158851792+07ronak@users.noreply.github.com>
2026-05-16 13:00:28 +0530

    Time: 48 ms (89.36%), Space: 56.1 MB (85.11%) - LeetHub

--- 0799-champagne-tower/0799-champagne-tower.js
+++ 0799-champagne-tower/0799-champagne-tower.js
@@ -7,19 +7,17 @@
 var champagneTower = function(poured, query_row, query_glass) {
     if(poured===0) return 0
 
-    let arr = Array.from({length:query_row+2},(_,i)=> new Array(i+1).fill(0))
+    let arr = new Array(query_row+1).fill(0)
 
-    arr[0][0] = poured
+    arr[0] = poured
 
-    for(let i=0; i<=query_row; i++){
-        for(let j=0; j<=i; j++){
-            let amount = arr[i][j] -1
-            if(amount>0){
-                arr[i+1][j] += amount/2
-                arr[i+1][j+1] += amount/2    
-            }
+    for(let i=0; i<query_row; i++){
+        for(let j=i; j>=0; j--){
+            let excess = Math.max(0,arr[j] - 1)
+            arr[j] = excess/2
+            arr[j+1] += excess/2
         }
     }
 
-    return Math.min(1,arr[query_row][query_glass])
+    return Math.min(1,arr[query_glass])
 };
