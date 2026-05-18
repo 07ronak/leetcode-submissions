c696679cc870e71a6f5903867a8a4e0f1d623074
Ronak Hingonia <158851792+07ronak@users.noreply.github.com>
2026-05-18 16:36:43 +0530

    Time: 0 ms (100%), Space: 53.1 MB (84.44%) - LeetHub

--- 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.js
+++ 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.js
@@ -1,16 +1,22 @@
-const numSteps = s => {
-    let steps = 0, carry = 0;
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var numSteps = function(s) {
+    let steps = 0
+    const n = s.length
+    let carry = 0
 
-    for (let i = s.length - 1; i > 0; i--) {
-        const bit = Number(s[i]);   // or s[i] === '1' ? 1 : 0
-        
-        if ((bit + carry) & 1) {
-            steps += 2;
-            carry = 1;
-        } else {
-            steps += 1;
+    for(let i=n-1; i>0; i--){
+        const num = Number(s[i]) + carry
+
+        if(num & 1){
+            carry = 1
+            steps+=2
+        } else{
+            steps +=1
         }
     }
 
-    return steps + carry;
+    return steps + carry
 };
