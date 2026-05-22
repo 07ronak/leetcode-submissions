e32c3dbf2dda8695bb60290888c594288e7cbbae
Ronak Hingonia <158851792+07ronak@users.noreply.github.com>
2026-05-22 14:14:11 +0530

    Time: 3 ms (73.47%), Space: 56.3 MB (61.22%) - LeetHub

---
+++ 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.js
@@ -0,0 +1,37 @@
+/**
+ * @param {number[][]} mat
+ * @return {number}
+ */
+var numSpecial = function(mat) {
+    const rows = mat.length
+    const cols = mat[0].length
+
+    const rowCount = new Array(rows).fill(0)
+    const colCount = new Array(cols).fill(0)
+
+    for(let i=0; i<rows; i++){
+        for(let j=0; j<cols; j++){
+            if(mat[i][j]===1){
+                rowCount[i]++
+                colCount[j]++
+            }
+        }
+    }
+
+    let count = 0
+
+    for(let i=0; i<rows; i++){
+        if(rowCount[i]===1){
+            for(let j=0; j<cols; j++){
+                if(mat[i][j]===1){
+                    if(colCount[j]===1){
+                        count++
+                        break
+                    }
+                }
+            }
+        }
+    }
+
+    return count
+};
