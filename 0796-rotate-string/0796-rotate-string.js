dab8addb29767fe8426fd9fd9896fc3299406ca0
Ronak Hingonia <158851792+07ronak@users.noreply.github.com>
2026-05-28 10:22:37 +0530

    Time: 0 ms (100%), Space: 53.1 MB (86.59%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.js
+++ 0796-rotate-string/0796-rotate-string.js
@@ -4,21 +4,7 @@
  * @return {boolean}
  */
 var rotateString = function(s, goal) {
-    const n = s.length
-    const m = goal.length
+    if(s.length!==goal.length) return false
 
-    if(m!==n) return false
-
-    for(let i=0; i<n; i++){
-        if(s[0]===goal[i]){
-            let j = 0
-            while(j<n){
-                if(s[j]!==goal[(i+j)%n]) break
-                j++
-            }
-            if(j===n) return true
-        }
-    }
-
-    return false
+    return (s+s).includes(goal)
 };
