67595762d33b734f958553e5f2990752d78dbcba
Ronak Hingonia <158851792+07ronak@users.noreply.github.com>
2026-06-01 12:50:18 +0530

    Time: 3 ms (90.91%), Space: 64.1 MB (86.36%) - LeetHub

--- 1664-ways-to-make-a-fair-array/1664-ways-to-make-a-fair-array.js
+++ 1664-ways-to-make-a-fair-array/1664-ways-to-make-a-fair-array.js
@@ -3,47 +3,48 @@
  * @return {number}
  */
 var waysToMakeFair = function(nums) {
-    const n = nums.length;
+    const n = nums.length
+    let count = 0
 
-    let totalEven = 0;
-    let totalOdd = 0;
+    let totalEven = 0
+    let totalOdd = 0
 
-    for (let i = 0; i < n; i++) {
-        if (i % 2 === 0) {
-            totalEven += nums[i];
-        } else {
-            totalOdd += nums[i];
+    for(let i=0; i<n; i++){
+        if(i&1){
+            totalOdd += nums[i]
+        } else{
+            totalEven += nums[i]
         }
     }
 
-    let leftEven = 0;
-    let leftOdd = 0;
-    let count = 0;
+    let leftEven = 0
+    let leftOdd = 0
 
-    for (let i = 0; i < n; i++) {
-        let rightEven, rightOdd;
+    for(let i=0; i<n; i++){
+        let rightEven;
+        let rightOdd;
 
-        if (i % 2 === 0) {
-            rightEven = totalEven - leftEven - nums[i];
-            rightOdd = totalOdd - leftOdd;
-        } else {
-            rightEven = totalEven - leftEven;
-            rightOdd = totalOdd - leftOdd - nums[i];
+        if(i&1){
+            rightEven = totalEven - leftEven
+            rightOdd = totalOdd - leftOdd - nums[i]
+        } else{
+            rightEven = totalEven - leftEven - nums[i]
+            rightOdd = totalOdd - leftOdd
         }
 
-        const newEven = leftEven + rightOdd;
-        const newOdd = leftOdd + rightEven;
+        const newEven = leftEven + rightOdd
+        const newOdd = leftOdd + rightEven
 
-        if (newEven === newOdd) {
-            count++;
+        if(newEven === newOdd){
+            count++
         }
 
-        if (i % 2 === 0) {
-            leftEven += nums[i];
-        } else {
-            leftOdd += nums[i];
+        if(i&1){
+            leftOdd += nums[i]
+        } else{
+            leftEven += nums[i]
         }
     }
 
-    return count;
+    return count
 };
