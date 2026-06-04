6d234bd81e885de19d53f7ffa3f02e9467188836
Ronak Hingonia <158851792+07ronak@users.noreply.github.com>
2026-06-04 13:32:55 +0530

    Time: 1 ms (96.79%), Space: 55.6 MB (94.19%) - LeetHub

--- 3633-earliest-finish-time-for-land-and-water-rides-i/3633-earliest-finish-time-for-land-and-water-rides-i.js
+++ 3633-earliest-finish-time-for-land-and-water-rides-i/3633-earliest-finish-time-for-land-and-water-rides-i.js
@@ -1,24 +1,39 @@
-var earliestFinishTime = function (
-    landStartTime,
-    landDuration,
-    waterStartTime,
-    waterDuration,
-) {
-    let n = landStartTime.length;
-    let m = waterStartTime.length;
-    let res = Infinity;
-    for (let i = 0; i < n; i++) {
-        for (let j = 0; j < m; j++) {
-            let land = landStartTime[i] + landDuration[i];
-            let land_water =
-                Math.max(land, waterStartTime[j]) + waterDuration[j];
-            res = Math.min(res, land_water);
+/**
+ * @param {number[]} landStartTime
+ * @param {number[]} landDuration
+ * @param {number[]} waterStartTime
+ * @param {number[]} waterDuration
+ * @return {number}
+ */
+var earliestFinishTime = function(landStartTime, landDuration, waterStartTime, waterDuration) {
+    const n = landStartTime.length
+    const m = waterStartTime.length
 
-            let water = waterStartTime[j] + waterDuration[j];
-            let water_land =
-                Math.max(water, landStartTime[i]) + landDuration[i];
-            res = Math.min(res, water_land);
-        }
+    //land-water
+    //find earliest land
+    let land = Infinity
+    for(let i=0; i<n; i++){
+        land = Math.min(land,landStartTime[i]+landDuration[i])
     }
-    return res;
+    //now use this to find minimum of land_water
+    let land_water = Infinity
+    for(let i=0; i<m; i++){
+        land_water = Math.min(land_water,Math.max(land,waterStartTime[i])+waterDuration[i])
+    }
+
+
+    //water-land
+    //find earliest water
+    let water = Infinity
+    for(let i=0; i<m; i++){
+        water = Math.min(water,waterStartTime[i]+waterDuration[i])
+    }
+    //now use this to find minimum of water_land
+    let water_land = Infinity
+    for(let i=0; i<n; i++){
+        water_land = Math.min(water_land,Math.max(water,landStartTime[i])+landDuration[i])
+    }
+
+
+    return Math.min(land_water,water_land)
 };
