3d849e2a1f028d21b17b7727efb9fcddbdf94db7
Ronak Hingonia <158851792+07ronak@users.noreply.github.com>
2026-06-04 13:40:50 +0530

    Time: 1 ms (96.79%), Space: 56.8 MB (55.31%) - LeetHub

--- 3633-earliest-finish-time-for-land-and-water-rides-i/3633-earliest-finish-time-for-land-and-water-rides-i.js
+++ 3633-earliest-finish-time-for-land-and-water-rides-i/3633-earliest-finish-time-for-land-and-water-rides-i.js
@@ -16,24 +16,18 @@ var earliestFinishTime = function(landStartTime, landDuration, waterStartTime, w
         land = Math.min(land,landStartTime[i]+landDuration[i])
     }
     //now use this to find minimum of land_water
-    let land_water = Infinity
-    for(let i=0; i<m; i++){
-        land_water = Math.min(land_water,Math.max(land,waterStartTime[i])+waterDuration[i])
-    }
-
-
-    //water-land
-    //find earliest water
+    let res = Infinity
     let water = Infinity
     for(let i=0; i<m; i++){
         water = Math.min(water,waterStartTime[i]+waterDuration[i])
+        res = Math.min(res,Math.max(land,waterStartTime[i])+waterDuration[i])
     }
-    //now use this to find minimum of water_land
-    let water_land = Infinity
+
+    //water-land
+    //now use `water` to find minimum of water_land
     for(let i=0; i<n; i++){
-        water_land = Math.min(water_land,Math.max(water,landStartTime[i])+landDuration[i])
+        res = Math.min(res,Math.max(water,landStartTime[i])+landDuration[i])
     }
 
-
-    return Math.min(land_water,water_land)
+    return res
 };
