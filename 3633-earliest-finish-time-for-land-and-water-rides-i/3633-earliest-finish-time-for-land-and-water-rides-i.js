e5fa4a305434ce399b62097a0949f14f4f4d3dac
Ronak Hingonia <158851792+07ronak@users.noreply.github.com>
2026-06-04 11:11:34 +0530

    Time: 6 ms (44.89%), Space: 56.1 MB (78.96%) - LeetHub

--- 3633-earliest-finish-time-for-land-and-water-rides-i/3633-earliest-finish-time-for-land-and-water-rides-i.js
+++ 3633-earliest-finish-time-for-land-and-water-rides-i/3633-earliest-finish-time-for-land-and-water-rides-i.js
@@ -1,42 +1,24 @@
-/**
- * @param {number[]} landStartTime
- * @param {number[]} landDuration
- * @param {number[]} waterStartTime
- * @param {number[]} waterDuration
- * @return {number}
- */
 var earliestFinishTime = function (
     landStartTime,
     landDuration,
     waterStartTime,
     waterDuration,
 ) {
-    function solve(start1, duration1, start2, duration2) {
-        let finish1 = Infinity;
-        for (let i = 0; i < start1.length; i++) {
-            finish1 = Math.min(finish1, start1[i] + duration1[i]);
-        }
-        let finish2 = Infinity;
-        for (let i = 0; i < start2.length; i++) {
-            finish2 = Math.min(
-                finish2,
-                Math.max(start2[i], finish1) + duration2[i],
-            );
-        }
-        return finish2;
-    }
+    let n = landStartTime.length;
+    let m = waterStartTime.length;
+    let res = Infinity;
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < m; j++) {
+            let land = landStartTime[i] + landDuration[i];
+            let land_water =
+                Math.max(land, waterStartTime[j]) + waterDuration[j];
+            res = Math.min(res, land_water);
 
-    let land_water = solve(
-        landStartTime,
-        landDuration,
-        waterStartTime,
-        waterDuration,
-    );
-    let water_land = solve(
-        waterStartTime,
-        waterDuration,
-        landStartTime,
-        landDuration,
-    );
-    return Math.min(land_water, water_land);
+            let water = waterStartTime[j] + waterDuration[j];
+            let water_land =
+                Math.max(water, landStartTime[i]) + landDuration[i];
+            res = Math.min(res, water_land);
+        }
+    }
+    return res;
 };
