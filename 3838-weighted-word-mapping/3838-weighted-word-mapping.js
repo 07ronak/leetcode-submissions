4feeb17905fd33301ac59fcbd1aa05ac3baaf612
Ronak Hingonia <158851792+07ronak@users.noreply.github.com>
2026-06-13 07:32:58 +0530

    Time: 1 ms (95.79%), Space: 57.5 MB (54.74%) - LeetHub

--- 3838-weighted-word-mapping/3838-weighted-word-mapping.js
+++ 3838-weighted-word-mapping/3838-weighted-word-mapping.js
@@ -4,17 +4,18 @@
  * @return {string}
  */
 var mapWordWeights = function(words, weights) {
-    let str = ""
+    let arr = []
 
-    
     for(let word of words){
         let sum = 0
         for(let c of word){
-            sum += (weights[c.charCodeAt(0)-97])
+            sum += weights[c.charCodeAt(0)-97]
         }
-        //console.log(sum)
-        str += String.fromCharCode(122 - (sum%26))
+        sum = sum % 26
+        sum = 25-sum
+
+        arr.push(String.fromCharCode(97 + sum))
     }
 
-    return str
+    return arr.join("")
 };
