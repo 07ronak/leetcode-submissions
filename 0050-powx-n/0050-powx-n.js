3c84baece0e39244063d2ede699f64f85d31a63a
Ronak Hingonia <158851792+07ronak@users.noreply.github.com>
2026-06-14 21:23:14 +0530

    Time: 0 ms (100%), Space: 53.2 MB (83.47%) - LeetHub

--- 0050-powx-n/0050-powx-n.js
+++ 0050-powx-n/0050-powx-n.js
@@ -4,29 +4,27 @@
  * @return {number}
  */
 var myPow = function(x, n) {
-    if(x===1) return 1
-    if (x === 0) return 0
-    if (n === 0) return 1
+    if(n===0) return 1
+    if(x===0) return 0
+    if(n===1) return x
     if(x===-1){
         if(n&1) return -1
         return 1
     }
-    
-    const helper = (x,n) =>{
-        if (n === 0) {
-            return 1;
-        }
 
-        const half = helper(x, Math.floor(n / 2));
+    const helper = (x,n) =>{
+        if(n===0) return 1
+
+        const half = helper(x,Math.floor(n/2))
 
         if(n&1){
-            return half * half * x
+            return half*half*x
         }
 
-        return half * half
+        return half*half
     }
 
     const res = helper(x,Math.abs(n))
 
-    return n >= 0 ? res : 1 / res;
+    return n>0 ? res : 1/res 
 };
