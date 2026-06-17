0a9dc2db346dce2c4a41ca34fcf4b646e5640f88
Ronak Hingonia <158851792+07ronak@users.noreply.github.com>
2026-06-17 21:58:21 +0530

    Time: 42 ms (60%), Space: 64.5 MB (100%) - LeetHub

--- 3614-process-string-with-special-operations-ii/3614-process-string-with-special-operations-ii.js
+++ 3614-process-string-with-special-operations-ii/3614-process-string-with-special-operations-ii.js
@@ -4,8 +4,8 @@
  * @return {character}
  */
 var processStr = function(s, k) {
-    let len = 0
     const n = s.length
+    let len = 0
 
     for(let c of s){
         if(c==="*"){
@@ -14,39 +14,33 @@ var processStr = function(s, k) {
             }
         } else if(c==="#"){
             len *= 2
-        } else if(c==="%"){
-            //do nothing
-        } else{
+        } else if(c!=="%"){
             len++
         }
     }
 
     if(k>=len) return "."
-    
 
-    for (let i = n - 1; i >= 0; i--) {
-        const c = s[i];
+    for(let i=n-1; i>=0; i--){
+        const c = s[i]
 
-        if (c === "*") {
-            len++;
-
-        } else if (c === "#") {
+        if(c==="*"){
+            len++
+        } else if(c==="#"){
             const half = Math.floor(len/2)
 
-            if (k >= half) {
+            if(k>=half){
                 k -= half
             }
+
             len = half
-
-        } else if (c === "%") {
-            k = len - k - 1;
-
-        } else {
-            if (k + 1 === len) {
-                return c;
+        } else if (c==="%"){
+            k = len - k - 1
+        } else{
+            if(k+1===len){
+                return c
             }
-            len--;
+            len--
         }
     }
-    
 };
