8d9c906cc0d44ef9309b0031a5e7e9ab95bdc599
Ronak Hingonia <158851792+07ronak@users.noreply.github.com>
2026-06-19 16:10:36 +0530

    Time: 0 ms (100%), Space: 53.6 MB (68.37%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
@@ -5,32 +5,41 @@
  */
 var search = function(nums, target) {
     let left = 0
-    let right = nums.length-1
-
+    let right = nums.length - 1
+    
     while(right>=left){
+        
+        if(nums[left]===target){
+            return left
+        }
+        
+        if(nums[right]===target){
+            return right
+        }
+        
         const mid = Math.floor((left+right)/2)
-
+        
         if(nums[mid]===target){
             return mid
         }
-
-
-        if(nums[mid]>=nums[left]){
-            //left part is sorted
-            if(target>=nums[left] && target<nums[mid]){
-                right = mid -1
+        
+        if(nums[mid]<nums[right]){
+            //right half is sorted
+            if(target<nums[right] && target>nums[mid]){
+                left = mid + 1
             } else{
-                left = mid+1
+                right = mid - 1
             }
         } else{
-            //right part is sorted
-            if(target<=nums[right] && target>nums[mid]){
+            //left half is sorted
+            if(target>nums[left] && target<nums[mid]){
+                right = mid - 1
+            } else {
                 left = mid +1
-            } else{
-                right = mid-1
             }
+            
         }
     }
-
+    
     return -1
 };
