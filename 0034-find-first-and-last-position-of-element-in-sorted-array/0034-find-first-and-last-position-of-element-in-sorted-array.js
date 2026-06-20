472da72767a2f24fdf6bb3eb79a777e586ac5414
Ronak Hingonia <158851792+07ronak@users.noreply.github.com>
2026-06-20 12:19:34 +0530

    Time: 0 ms (100%), Space: 54.9 MB (36.52%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.js
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.js
@@ -3,39 +3,41 @@
  * @param {number} target
  * @return {number[]}
  */
-/**
- * @param {number[]} nums
- * @param {number} target
- * @return {number[]}
- */
 var searchRange = function(nums, target) {
-    const binarySearch = (nums, target, isSearchingLeft) => {
-        let left = 0;
-        let right = nums.length - 1;
-        let idx = -1;
-      
-        while (left <= right) {
-            const mid = Math.floor((left + right) / 2);
-            
-            if (nums[mid] > target) {
-                right = mid - 1;
-            } else if (nums[mid] < target) {
-                left = mid + 1;
-            } else {
-                idx = mid;
-                if (isSearchingLeft) {
-                    right = mid - 1;
-                } else {
-                    left = mid + 1;
-                }
-            }
+    let first = -1;
+    let last = -1;
+    const n = nums.length
+
+    // find first occurrence
+    let l = 0, r = n - 1;
+    while (l <= r) {
+        const mid = Math.floor((l + r) / 2);
+
+        if (nums[mid] === target) {
+            first = mid;
+            r = mid - 1;   // keep searching left
+        } else if (nums[mid] < target) {
+            l = mid + 1;
+        } else {
+            r = mid - 1;
         }
-      
-        return idx;
-    };
-    
-    const left = binarySearch(nums, target, true);
-    const right = binarySearch(nums, target, false);
-    
-    return [left, right];    
+    }
+
+    // find last occurrence
+    l = 0;
+    r = n - 1;
+    while (l <= r) {
+        const mid = Math.floor((l + r) / 2);
+
+        if (nums[mid] === target) {
+            last = mid;
+            l = mid + 1;   // keep searching right
+        } else if (nums[mid] < target) {
+            l = mid + 1;
+        } else {
+            r = mid - 1;
+        }
+    }
+
+    return [first, last];
 };
