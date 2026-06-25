e50c67fb1a995ce09c8cc1506fd7104d0fe9b0b3
Ronak Hingonia <158851792+07ronak@users.noreply.github.com>
2026-06-25 16:13:48 +0530

    Time: 45 ms (11.11%), Space: 56.7 MB (94.44%) - LeetHub

---
+++ 3737-count-subarrays-with-majority-element-i/3737-count-subarrays-with-majority-element-i.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var countMajoritySubarrays = function(nums, target) {
+    const n = nums.length
+    let ans = 0
+
+    for(let i=0; i<n; i++){
+        let count = 0
+        for(let j=i; j<n; j++){
+            if(nums[j]===target){
+                count++
+            }
+
+            if(count > Math.floor((j-i+1)/2)) ans++
+        }
+    }
+
+    return ans
+};
