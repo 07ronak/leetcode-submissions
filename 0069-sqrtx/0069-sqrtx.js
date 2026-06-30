e4d51b932812759b3b38280afb0c4dfcba8c109a
Ronak Hingonia <158851792+07ronak@users.noreply.github.com>
2026-06-30 14:20:57 +0530

    Time: 2 ms (37.55%), Space: 56.5 MB (32.79%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.js
+++ 0069-sqrtx/0069-sqrtx.js
@@ -5,25 +5,23 @@
 var mySqrt = function(x) {
     if(x<=1) return x
 
-    let max = 1
     let left = 1
     let right = x
+    let ans = 1
 
     while(right>=left){
-        const mid = Math.floor((left+right)/2)
-        const val = mid*mid
+        const mid = left + Math.floor((right-left)/2)
+        const val = mid * mid
 
-        if(val===x){
-            return mid
-        }
+        if(val===x) return mid
 
-        if(val < x){
-            max = Math.max(max,mid)
-            left = mid +1
-        } else{
-            right = mid -1
+        if(val<x){
+            ans = mid
+            left = mid + 1
+        } else {
+            right = mid - 1
         }
     }
 
-    return max
+    return ans
 };
