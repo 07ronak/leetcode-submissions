281ad46f6375c21dad0de5055931df6c6a3026c2
Ronak Hingonia <158851792+07ronak@users.noreply.github.com>
2026-07-01 15:48:35 +0530

    Time: 1 ms (99.61%), Space: 57.4 MB (90.09%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.js
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.js
@@ -7,40 +7,37 @@ var findMedianSortedArrays = function(nums1, nums2) {
     const n = nums1.length
     const m = nums2.length
 
-    //force nums1 to be the smaller one
-    if(m<n){
+    //force nums1 to be the smaller array
+    if(n > m){
         return findMedianSortedArrays(nums2,nums1)
     }
 
     const total = n + m
     const half = Math.floor(total/2)
 
-    //choosing the smaller array that is nums1 in our case
-
     let left = 0
-    let right = n-1
-    
-    while(true){
-        const mid = Math.floor((left+right)/2)
-        const j = half - mid -2
+    let right = n
 
-        const Aleft = nums1[mid] ?? -Infinity
-        const Aright = nums1[mid+1] ?? Infinity
-        const Bleft = nums2[j] ?? -Infinity
-        const Bright = nums2[j+1] ?? Infinity
+    while(right>=left){
+        const mid = left + Math.floor((right-left)/2)
+        const j = half - mid
+
+        const Aleft = nums1[mid-1] ?? -Infinity
+        const Aright = nums1[mid] ?? Infinity
+        const Bleft = nums2[j-1] ?? -Infinity
+        const Bright = nums2[j] ?? Infinity
 
         if(Aleft<=Bright && Bleft<=Aright){
-            if(total&1){
+            //found the answer
+            if(total & 1){
                 return Math.min(Aright,Bright)
             } else{
-                return (Math.max(Aleft,Bleft) + Math.min(Aright, Bright))/2
+                return (Math.max(Aleft,Bleft) + Math.min(Aright,Bright))/2
             }
-        }
-
-        if(Aleft>Bright){
+        } else if(Aleft>Bright){
             right = mid -1
         } else{
-            left = mid +1
+            left = mid + 1
         }
     }
-};  
+};
