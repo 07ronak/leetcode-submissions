ccd358b91b849b6011f7d2c54124bc75c83e7f04
Ronak Hingonia <158851792+07ronak@users.noreply.github.com>
2026-07-12 22:28:47 +0530

    Time: 1 ms (96.95%), Space: 58.7 MB (89.01%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.js
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.js
@@ -1,39 +1,32 @@
 /**
- * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
- * }
- */
+* Definition for a binary tree node.
+* function TreeNode(val, left, right) {
+*     this.val = (val===undefined ? 0 : val)
+*     this.left = (left===undefined ? null : left)
+*     this.right = (right===undefined ? null : right)
+* }
+*/
 /**
- * @param {number[]} preorder
- * @param {number[]} inorder
- * @return {TreeNode}
- */
-var buildTree = function (preorder, inorder) {
-    const n = inorder.length
-    const map = new Map()
-
-    for (let i = 0; i < n; i++) {
-        map.set(inorder[i], i)
+* @param {number[]} preorder
+* @param {number[]} inorder
+* @return {TreeNode}
+*/
+var buildTree = function(preorder, inorder) {
+   let preIdx = 0, inIdx = 0;
+    
+    function dfs(limit) {
+        if (preIdx >= preorder.length) return null;
+        if (inorder[inIdx] === limit) {
+            inIdx++;
+            return null;
+        }
+            
+        let root = new TreeNode(preorder[preIdx++]);
+        root.left = dfs(root.val);
+        root.right = dfs(limit);
+        return root;
+        
     }
-
-    let idx = 0
-
-    const helper = (l, r) => {
-        if (l > r) return null
-
-        const rootVal = preorder[idx++]
-        const root = new TreeNode(rootVal)
-
-        const mid = map.get(rootVal)
-
-        root.left = helper(l, mid - 1)
-        root.right = helper(mid + 1, r)
-
-        return root
-    }
-
-    return helper(0, n - 1)
+    return dfs(Infinity);
+    
 };
