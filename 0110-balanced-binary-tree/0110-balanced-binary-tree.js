ffd505cd5af0254f3392607ceed12579c3b6401b
Ronak Hingonia <158851792+07ronak@users.noreply.github.com>
2026-07-22 12:46:39 +0530

    Time: 1 ms (83.91%), Space: 60.7 MB (19.23%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.js
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.js
@@ -1,28 +1,19 @@
-/**
- * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
- * }
- */
-/**
- * @param {TreeNode} root
- * @return {boolean}
- */
 var isBalanced = function (root) {
-    if (!root) return true
+    if (!root) return true;
 
     const dfs = (node) => {
-        if (!node) return [true, 0]
+        if (!node) return [true, 0];
 
-        const left = dfs(node.left)
-        const right = dfs(node.right)
+        const left = dfs(node.left);
+        if (!left[0]) return [false, 1];
 
-        let balanced = left[0] && right[0] && (Math.abs(left[1] - right[1]) <= 1)
+        const right = dfs(node.right);
+        if (!right[0]) return [false, 1];
 
-        return [balanced, 1 + Math.max(left[1], right[1])]
-    }
+        const balanced = Math.abs(left[1] - right[1]) <= 1;
 
-    return dfs(root)[0]
+        return [balanced, 1 + Math.max(left[1], right[1])];
+    };
+
+    return dfs(root)[0];
 };
