3db281b724c0d68e82b7c1e7afdc7b0c930f0f8c
Ronak Hingonia <158851792+07ronak@users.noreply.github.com>
2026-07-26 11:50:52 +0530

    Time: 9 ms (79.02%), Space: 61 MB (5.36%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.js
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.js
@@ -3,8 +3,41 @@
  * @return {number}
  */
 var maximumProduct = function (nums) {
-    const n = nums.length
-    nums.sort((a, b) => a - b)
+    const arr = [-Infinity, -Infinity, -Infinity]
 
-    return Math.max((nums[0]*nums[1]*nums[n-1]),(nums[n-1]*nums[n-2]*nums[n-3]))
+    const insert = (num) => {
+        if (num > arr[0]) {
+            arr[2] = arr[1]
+            arr[1] = arr[0]
+            arr[0] = num
+        } else if (num > arr[1]) {
+            arr[2] = arr[1]
+            arr[1] = num
+        } else {
+            arr[2] = num
+        }
+    }
+
+    const neg = [Infinity, Infinity]
+
+    const insert2 = (num) => {
+        if (num < neg[0]) {
+            neg[1] = neg[0]
+            neg[0] = num
+        } else {
+            neg[1] = num
+        }
+    }
+
+    for (const num of nums) {
+        if (num > arr[2]) {
+            insert(num)
+        }
+
+        if (num < neg[1]) {
+            insert2(num)
+        }
+    }
+
+    return Math.max((arr[0] * arr[1] * arr[2]), (arr[0] * neg[0] * neg[1]))
 };
