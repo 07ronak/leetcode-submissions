9bb378258b71b4a9e8363f4bb6f1b9ee8ef5fd37
Ronak Hingonia <158851792+07ronak@users.noreply.github.com>
2026-08-03 14:34:36 +0530

    Time: 15 ms (100%), Space: 63.6 MB (100%) - LeetHub

---
+++ 1406-stone-game-iii/1406-stone-game-iii.js
@@ -0,0 +1,17 @@
+const s = ["Bob", "Tie", "Alice"];
+const stoneGameIII = A => {
+    const n = A.length;
+    const dp = [0, 0, 0, 0];
+
+    for (let i = n - 1; i >= 0; i--) {
+        dp[i & 3] = -5e7;
+        let sum = 0;
+
+        for (let j = 1; j <= 3 && i + j <= n; j++) {
+            sum += A[i + j - 1];
+            dp[i & 3] = Math.max(dp[i & 3], sum - dp[(i + j) & 3]);
+        }
+    }
+
+    return s[(dp[0] > 0) - (dp[0] < 0) + 1];
+};
