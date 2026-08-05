91905f3ac0b4df5755113b439c5e9ef718e666bc
Ronak Hingonia <158851792+07ronak@users.noreply.github.com>
2026-08-05 10:47:51 +0530

    Time: 2 ms (79.52%), Space: 57.9 MB (95.22%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.js
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.js
@@ -1,4 +1,12 @@
 /**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
  * @param {number[]} inorder
  * @param {number[]} postorder
  * @return {TreeNode}
@@ -11,21 +19,18 @@ var buildTree = function (inorder, postorder) {
         map.set(inorder[i], i)
     }
 
-    let idx = n - 1
-
-    const helper = (l, r) => {
+    const dfs = (l, r) => {
         if (l > r) return null
 
-        const rootVal = postorder[idx--]
-        const root = new TreeNode(rootVal)
+        const val = postorder.pop()
+        const root = new TreeNode(val)
+        const mid = map.get(val)
 
-        const mid = map.get(rootVal)
-
-        root.right = helper(mid + 1, r)
-        root.left = helper(l, mid - 1)
+        root.right = dfs(mid + 1, r)
+        root.left = dfs(l, mid - 1)
 
         return root
     }
 
-    return helper(0, idx)
+    return dfs(0, n - 1)
 };
