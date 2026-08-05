8f0a4cba0704480d5d15afc32ab75394a9e613ba
Ronak Hingonia <158851792+07ronak@users.noreply.github.com>
2026-08-05 10:52:50 +0530

    Time: 1 ms (96.77%), Space: 59.3 MB (76.89%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.js
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.js
@@ -1,32 +1,39 @@
 /**
-* Definition for a binary tree node.
-* function TreeNode(val, left, right) {
-*     this.val = (val===undefined ? 0 : val)
-*     this.left = (left===undefined ? null : left)
-*     this.right = (right===undefined ? null : right)
-* }
-*/
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
 /**
-* @param {number[]} preorder
-* @param {number[]} inorder
-* @return {TreeNode}
-*/
+ * @param {number[]} preorder
+ * @param {number[]} inorder
+ * @return {TreeNode}
+ */
 var buildTree = function(preorder, inorder) {
-   let preIdx = 0, inIdx = 0;
+    const n = inorder.length
+    const map = new Map()
     
-    function dfs(limit) {
-        if (preIdx >= preorder.length) return null;
-        if (inorder[inIdx] === limit) {
-            inIdx++;
-            return null;
-        }
-            
-        let root = new TreeNode(preorder[preIdx++]);
-        root.left = dfs(root.val);
-        root.right = dfs(limit);
-        return root;
-        
+    for(let i=0; i<n; i++){
+        map.set(inorder[i],i)
     }
-    return dfs(Infinity);
     
+    let idx = 0
+    
+    const dfs = (l,r) =>{
+        if(l>r) return null
+        
+        const val = preorder[idx++]
+        const root = new TreeNode(val)
+        
+        const mid = map.get(val)
+        
+        root.left = dfs(l,mid-1)
+        root.right = dfs(mid+1,r)
+        
+        return root
+    }
+    
+    return dfs(0,n-1)
 };
