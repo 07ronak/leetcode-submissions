746dfe7badc5a788c2605b1378b2528cc5aca9fb
Ronak Hingonia <158851792+07ronak@users.noreply.github.com>
2026-08-06 15:25:55 +0530

    Time: 44 ms (95.79%), Space: 59.6 MB (51.23%) - LeetHub

--- 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.js
+++ 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.js
@@ -1,26 +1,39 @@
-var connect = function(root) {
-    if (!root) return null;
+/**
+ * // Definition for a _Node.
+ * function _Node(val, left, right, next) {
+ *    this.val = val === undefined ? null : val;
+ *    this.left = left === undefined ? null : left;
+ *    this.right = right === undefined ? null : right;
+ *    this.next = next === undefined ? null : next;
+ * };
+ */
 
-    function getNextChild(node) {
-        while (node) {
-            if (node.left) return node.left;
-            if (node.right) return node.right;
-            node = node.next;
-        }
-        return null;
+/**
+ * @param {_Node} root
+ * @return {_Node}
+ */
+var connect = function (root) {
+    if (!root) return null
+
+    if (root.right) {
+        root.right.next = getNext(root.next)
     }
 
     if (root.left) {
-        root.left.next = root.right || getNextChild(root.next);
+        root.left.next = root.right ?? getNext(root.next)
     }
 
-    if (root.right) {
-        root.right.next = getNextChild(root.next);
-    }
+    connect(root.right)
+    connect(root.left)
 
-    // Right first, then left
-    connect(root.right);
-    connect(root.left);
-
-    return root;
+    return root
 };
+
+function getNext(node) {
+    while (node) {
+        if (node.left) return node.left
+        if (node.right) return node.right
+        node = node.next
+    }
+    return null
+}
