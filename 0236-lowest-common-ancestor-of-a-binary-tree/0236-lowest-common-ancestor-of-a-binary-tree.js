c373905e38bb84fbe9ebd1ffe120567dcb85b2f3
Ronak Hingonia <158851792+07ronak@users.noreply.github.com>
2026-08-06 15:58:59 +0530

    Time: 54 ms (99.32%), Space: 65.3 MB (97.95%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.js
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.js
@@ -12,14 +12,16 @@
  * @return {TreeNode}
  */
 var lowestCommonAncestor = function(root, p, q) {
-    if(!root || root===p || root===q) return root
-    
-    const left = lowestCommonAncestor(root.left,p,q)
-    const right = lowestCommonAncestor(root.right,p,q)
-    
-    if(left && right){
-        return root
+    if (!root || root === p || root === q) {
+        return root;
     }
-    
-    return (left || right) 
+
+    const left = lowestCommonAncestor(root.left, p, q);
+    const right = lowestCommonAncestor(root.right, p, q);
+
+    if (left && right) {
+        return root;
+    }
+
+    return left || right;    
 };
