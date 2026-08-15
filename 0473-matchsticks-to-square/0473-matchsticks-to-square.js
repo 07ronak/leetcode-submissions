eb8adb7776524dfd4f7a84e0166e1989abe37454
Ronak Hingonia <158851792+07ronak@users.noreply.github.com>
2026-08-15 12:43:30 +0530

    Time: 7 ms (85.42%), Space: 54.1 MB (86.46%) - LeetHub

--- 0473-matchsticks-to-square/0473-matchsticks-to-square.js
+++ 0473-matchsticks-to-square/0473-matchsticks-to-square.js
@@ -14,15 +14,21 @@ var makesquare = function (matchsticks) {
 
     function bt(idx) {
         if (idx === n) {
-            return true
+            return true //this can be said because of check 1
+            //we have divided the total sum by 4. that means each side must be of lenght - side
+            // since we are never allowing to exceed that limit, we will reach end only when each side is equal
+            // why?
+            // say a side remains smaller than 'side', we would still continue with that ofc becuase it is not more
+            //but since we divided the total into equal 4, some other stick will definitely cross the 'side' length mark, resulting false
+            //thus, when we reach the end, all sides were equal, that's the only possibility
         }
 
         const val = matchsticks[idx];
 
         for (let i = 0; i < 4; i++) {
-            if (sides[i] + val > side) continue;
+            if (sides[i] + val > side) continue; //check 1
 
-            if (i > 0 && sides[i] === sides[i - 1]) continue;
+            if (i > 0 && sides[i] === sides[i - 1]) continue; //just removing repeated checks
 
             sides[i] += val;
 
@@ -32,8 +38,8 @@ var makesquare = function (matchsticks) {
 
             // Symmetry pruning
             // this means that even putting this stick on empty side didn't work
-            // therefore, it cannot work
-            if (sides[i] === 0) break;
+            // therefore, it cannot work ....```read solution note```
+            if (sides[i] === 0) return false
         }
 
         return false;
