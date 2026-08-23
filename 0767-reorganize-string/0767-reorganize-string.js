d0ca9db80e22305a3e372dabc7d9fa1ff3f950b2
Ronak Hingonia <158851792+07ronak@users.noreply.github.com>
2026-08-23 14:03:57 +0530

    Time: 0 ms (100%), Space: 53.5 MB (93.03%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.js
+++ 0767-reorganize-string/0767-reorganize-string.js
@@ -5,42 +5,41 @@
 var reorganizeString = function (s) {
     const n = s.length
     const count = new Array(26).fill(0)
-
+    let idx = -1
     let maxf = 0
-    let i = -1
     let maxChar = null
 
-    for (const char of s) {
-        const idx = char.charCodeAt(0) - 97
-        count[idx]++
-        if (count[idx] > maxf) {
-            maxf = count[idx]
-            i = idx
-            maxChar = char
+    for (const c of s) {
+        const i = c.charCodeAt(0) - 97
+        count[i]++
+
+        if (count[i] > maxf) {
+            maxf = count[i]
+            maxChar = c
+            idx = i
         }
     }
 
     if ((n - maxf) < (maxf - 1)) return ""
 
+    const res = new Array(n).fill("")
     let pos = 0
-    const res = new Array(n)
 
-    while (count[i] > 0) {
+    while (count[idx]) {
         res[pos] = maxChar
-
-        count[i]--
+        count[idx]--
         pos += 2
     }
 
     for (let i = 0; i < 26; i++) {
-        while (count[i] > 0) {
+        while (count[i]) {
             if (pos >= n) {
                 pos = 1
             }
 
             res[pos] = String.fromCharCode(i + 97)
-            count[i]--
             pos += 2
+            count[i]--
         }
     }
 
