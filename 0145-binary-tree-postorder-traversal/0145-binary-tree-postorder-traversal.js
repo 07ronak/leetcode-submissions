570febe7096fa77e733d46f22ceedce93c133311
Ronak Hingonia <158851792+07ronak@users.noreply.github.com>
2026-08-25 12:44:31 +0530

    Time: 1 ms (3.82%), Space: 53.8 MB (45.37%) - LeetHub

---
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.js
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var postorderTraversal = function (root) {
+    if (!root) return [];
+    const res = []
+
+    const stack = [root]
+    const visit = [false]
+
+    while (stack.length) {
+        const node = stack.pop()
+        const visited = visit.pop()
+
+        if (node) {
+            if (visited) {
+                res.push(node.val)
+            } else {
+                //visit the node again but mark it true this time
+                stack.push(node)
+                visit.push(true)
+
+                //do children
+                stack.push(node.right)
+                visit.push(false)
+                stack.push(node.left)
+                visit.push(false)
+            }
+        }
+    }
+
+    return res
+};
