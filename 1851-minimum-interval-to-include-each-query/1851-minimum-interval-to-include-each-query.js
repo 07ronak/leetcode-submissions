f73d0a7681a5d2fa07502867609ab59664da5a39
Ronak Hingonia <158851792+07ronak@users.noreply.github.com>
2026-08-31 13:49:27 +0530

    Time: 387 ms (13.56%), Space: 130.5 MB (5.09%) - LeetHub

--- 1851-minimum-interval-to-include-each-query/1851-minimum-interval-to-include-each-query.js
+++ 1851-minimum-interval-to-include-each-query/1851-minimum-interval-to-include-each-query.js
@@ -5,9 +5,6 @@
  */
 var minInterval = function (intervals, queries) {
     intervals.sort((a, b) => {
-        if (a[0] === b[0]) {
-            return a[1] - b[1]
-        }
         return a[0] - b[0]
     })
 
