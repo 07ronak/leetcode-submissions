26fbbc5a5b45107755d8aa903c77c4ec0c88c853
Ronak Hingonia <158851792+07ronak@users.noreply.github.com>
2026-09-04 14:56:02 +0530

    Time: 30 ms (89.79%), Space: 72.7 MB (41.41%) - LeetHub

--- 0015-3sum/0015-3sum.js
+++ 0015-3sum/0015-3sum.js
@@ -2,33 +2,34 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
-var threeSum = function(nums) {
-    nums.sort((a,b)=>a-b)
+var threeSum = function (nums) {
     const n = nums.length
-    let ans = []
+    nums.sort((a, b) => a - b)
+    const ans = []
 
-    for(let i=0; i<n-2; i++){
-        if(i>0 && nums[i]===nums[i-1]) continue
+    for (let i = 0; i < n - 2; i++) {
+        if (i > 0 && nums[i] === nums[i - 1]) continue
 
-        if(nums[i]+ nums[i+1] + nums[i+2] > 0) break
-        if(nums[i] + nums[n-1] + nums[n-2] < 0) continue
+        if (nums[i] + nums[i + 1] + nums[i + 2] > 0) break
+        if (nums[i] + nums[n - 1] + nums[n - 1] < 0) continue
 
-        let j = i+1
-        let k = n-1
+        let j = i + 1
+        let k = n - 1
 
-        while(k>j){
-            let val = nums[i] + nums[j] + nums[k]
-            if(val===0){
-                ans.push([nums[i],nums[j],nums[k]])
+        while (k > j) {
+            const val = nums[i] + nums[j] + nums[k]
 
-                while(k>j && nums[j]===nums[j+1]) j++
-                while(k>j && nums[k]===nums[k-1]) k--
+            if (val === 0) {
+                ans.push([nums[i], nums[j], nums[k]])
 
+                while (k > j && nums[k] === nums[k - 1]) k--
+                while (k > j && nums[j] === nums[j + 1]) j++
+
+                k--
                 j++
+            } else if (val > 0) {
                 k--
-            } else if(val>0){
-                k--
-            } else{
+            } else {
                 j++
             }
         }
