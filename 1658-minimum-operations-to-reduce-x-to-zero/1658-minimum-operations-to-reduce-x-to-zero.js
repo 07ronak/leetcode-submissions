7bb33c196e88ccee357040b1055b0b492231defa
Ronak Hingonia <158851792+07ronak@users.noreply.github.com>
2026-09-23 11:30:57 +0530

    Time: 69 ms (12.25%), Space: 84.3 MB (8.16%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.js
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.js
@@ -11,50 +11,43 @@ var minOperations = function (nums, x) {
     let steps = 0
     let min = Infinity
 
+    // Store prefix sums
     for (let i = 0; i < n; i++) {
         sum += nums[i]
-
-        if (sum === x) {
-            steps++
-            min = Math.min(min, steps)
-            break
-        }
-
-        if (sum > x) {
-            break
-        } else {
-            steps++
-            map.set(sum, steps)
-        }
-    }
-
-    steps = 0
-    sum = 0
-
-    for (let i = n - 1; i >= 0; i--) {
-        sum += nums[i]
-
-        if (sum === x) {
-            steps++
-            min = Math.min(min, steps)
-            break
-        }
+        steps++
 
         if (sum > x) break
 
-        const comp = x - sum
+        map.set(sum, steps)
+
+        if (sum === x) {
+            min = steps
+            break
+        }
+    }
+
+    // Check suffix + prefix combinations
+    sum = 0
+    steps = 0
+
+    for (let i = n - 1; i >= 0; i--) {
+        sum += nums[i]
         steps++
 
-        if (map.has(comp)) {
-            const left = map.get(comp)
+        if (sum > x) break
 
-            if (left + steps > n) {
-                break
-            } else {
-                min = Math.min(min, left + steps)
-            }
+        if (sum === x) {
+            min = Math.min(min, steps)
+            break
+        }
+
+        const prefixSteps = map.get(x - sum)
+
+        if (prefixSteps !== undefined && prefixSteps + steps <= n) {
+            min = Math.min(min, prefixSteps + steps)
         }
     }
 
     return min === Infinity ? -1 : min
-};
+}
+
